11302ceb6c1db7f471c15c7a6862cf17610c12f7
samiyonas <samuel.x.yonas@gmail.com>
2024-05-19 06:47:09 -0700

    many to many

--- models/place.py
+++ models/place.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.amenity import Amenity
+from os import getenv
 from models.review import Review
 from models.base_model import BaseModel, Base
-from models import storage
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.sql.schema import Table
 from sqlalchemy.orm import relationship
 
 
-if storage_type == 'db':
+if getenv("HBNB_TYPE_STORAGE") == 'db':
     place_amenity = Table('place_amenity', Base.metadata,
                           Column('place_id', String(60),
                                  ForeignKey('places.id'),
@@ -25,7 +25,7 @@ if storage_type == 'db':
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
-    if storage_type == 'db':
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
