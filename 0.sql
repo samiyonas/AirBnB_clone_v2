20dc25cdb959154987fb63e4221f6008d48b2886
samiyonas <samuel.x.yonas@gmail.com>
2024-05-19 02:59:54 -0700

    try again

---
+++ 0.sql
@@ -0,0 +1,5 @@
+-- Doc
+USE hbnb_dev_db;
+INSERT INTO users (id, email, password, first_name, created_at, updated_at) VALUES ("my_id_1", "email1@gmail.com", "pwd1", "Bob", CURDATE(), CURDATE());
+INSERT INTO users (id, email, password, last_name, created_at, updated_at) VALUES ("my_id_2", "email2@gmail.com", "pwd2", "Did", CURDATE(), CURDATE());
+INSERT INTO users (id, email, password, created_at, updated_at) VALUES ("my_id_3", "email3@gmail.com", "pwd3", CURDATE(), CURDATE());
