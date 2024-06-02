a98e964f9973801cf816ec6269f8c9e059a7f166
samiyonas <samuel.x.yonas@gmail.com>
2024-06-02 05:19:53 +0300

    preparing my web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# preparing my web servers
+
+if [ ! -x usr/sbin/nginx ]; then
+    sudo apt-get update -y -qq && sudo apt-get install -y nginx
+fi
+
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+fake_html=\
+"
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+ubuntu@89-web-01:~/$ curl localhost/hbnb_static/index.html
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+"
+
+sudo chown -R ubuntu:ubuntu /data
+
+echo "$fake_html" > /data/web_static/releases/test/index.html
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+sudo sed -i '51i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+sudo service nginx restart
