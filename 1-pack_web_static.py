6fadfb63a834813b40c61ad4ea00ba19e6005e13
samiyonas <samuel.x.yonas@gmail.com>
2024-06-02 07:50:18 -0700

    pack it up

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,15 +4,11 @@ from datetime import datetime
 from fabric.api import local
 
 
-@task
 def do_pack():
     """ pack the web static """
-    local("mkdir -p /versions")
+    local("mkdir -p versions")
     time = datetime.now().strftime("%Y%m%d%H%M")
-    arcf = "/versions/web_static_{}.tgz".format(time)
-    result = local(f"tar -czvf {arcf} /web_static/", capture=True)
+    arcf = "versions/web_static_{}.tgz".format(time)
+    result = local("tar -cvzf {} web_static".format(arcf), capture=True)
 
-    if result.failed:
-        return None
-    else:
-        return arcf
+    return arcf
