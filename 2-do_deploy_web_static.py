c33b2c617d7131129f8c48410dbad9c31c54d986
samiyonas <samuel.x.yonas@gmail.com>
2024-06-03 08:11:42 -0700

    deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,24 +19,23 @@ def do_pack():
 
     return arcf
 
+
 def do_deploy(archive_path):
     """ deploy the web static succesfully """
     if os.path.exists(archive_path) is False:
         return False
     try:
-        run("mkdir -p /tmp/")
-        put(archive_path, "/tmp/")
-        arch_dir = archive_path.split("/")[1].split(".")[0]
-        hp = "/data/web_static/releases/{}/web_static/*".format(arch_dir)
-        ft = archive_path.split("/")[1]
-        full_path = "/data/web_static/releases/{}".format(arch_dir)
-        run("mkdir -p {}".format(full_path))
-        run("tar -xzvf /tmp/{} -C {}".format(ft, full_path))
-        run("rm -rf /tmp/{}".format(ft))
-        run("mv {} /data/web_static/releases/{}/".format(hp, arch_dir))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(arch_dir))
-        run("rm -rf /data/web_static/current/")
-        run("ln -s {} /data/web_static/current".format(full_path))
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except Exception:
+    except:
         return False
