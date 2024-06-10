739e400e90060b6b5fbcc2abe771685b6a389a83
samiyonas <samuel.x.yonas@gmail.com>
2024-06-10 15:51:17 +0300

    python is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """ home page (kind of) """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ hbnb url """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text=None):
+    """ C is fun """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+@app.route("/python")
+@app.route("/python/")
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """ python is also cool """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
