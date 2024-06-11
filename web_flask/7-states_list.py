2b159dacfd126542dd484b29d49f11e8cb6cc1ac
samiyonas <samuel.x.yonas@gmail.com>
2024-06-12 00:20:50 +0300

    list of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -16,5 +16,11 @@ def states_list():
     return render_template("7-states_list.html", states=states)
 
 
+@app.teardown_appcontext
+def clean_up():
+    """ cleaning up """
+    storage.close()
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
