6bcc4087d7bd6a4064d1450cea1a23ad5632c52a
samiyonas <samuel.x.yonas@gmail.com>
2024-06-11 13:55:09 +0300

    improve engine

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.place import Place
 from models.review import Review
@@ -15,5 +15,4 @@ class User(BaseModel, Base):
     first_name = Column(String(128), nullable=True, default=None)
     last_name = Column(String(128), nullable=True, default=None)
     places = relationship("Place", backref="user", cascade="all, delete")
-    place_id = Column(String(60), ForeignKey('places.id'), nullable=True)
     reviews = relationship("Review", backref="user", cascade="all, delete")
