27fbe1dfd9d96e54a4d73cfdf425cb1ebb52bf83
samiyonas <samuel.x.yonas@gmail.com>
2024-06-11 14:03:47 +0300

    improve engine

--- models/place.py
+++ models/place.py
@@ -41,6 +41,18 @@ class Place(BaseModel, Base):
         amenities = relationship('Amenity', secondary=place_amenity,
                                  viewonly=False, backref='place_amenities')
     else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude =  0.0
+        amenity_ids = []
+
         @property
         def reviews(self):
             ''' returns list of review instances with place_id
